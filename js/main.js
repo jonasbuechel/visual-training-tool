5a40a7203b47236caab2cb80e85a867451fee2a5
jonasbuechel <jonas@frontify.com>
2020-08-12 08:49:53 +0200

    Use only JavaScript to access the svg, remove PHP as it's not needed anymore

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
+/* CONFIGURATION */
 const circleColorVisible = 'rgba(0,0,255,1)';
 const circleColorInvisible = 'rgba(0,0,0,0)';
-const shape = document.querySelector('#shape');
-const allCircles = shape.querySelectorAll('circle');
 let addMispositioning = false;
 let currentCircle = 0;
+let allCircles = null;
+
+/* GET THE SVG */
+window.addEventListener("load", () => {
+  loadShape('shape');
+  showCircle(currentCircle);
+});
+
+function loadShape(id) {
+  const svgObject = document.getElementById('svg-object').contentDocument;
+  const shape = svgObject.getElementById(id);
+  allCircles = shape.querySelectorAll('circle');
+}
+
 
 function showCircle(circleNumber) {
   const oldCircle = allCircles[currentCircle];
@@ -38,8 +51,6 @@ debugger;
 function getRandomNumber(maximum, minimum = 0) {
   return Math.floor(Math.random() * maximum) + minimum;
 }
-
-showCircle(currentCircle);
 
 document.addEventListener('keypress', () => {
   let circleNumber = currentCircle + 1;
