46d1a96a1800ac7b64b3efc7017cabada0a81157
jonasbuechel <jonas@frontify.com>
2020-08-15 11:09:58 +0200

    helpers: add debounce function

--- js/modules/helpers.js
+++ js/modules/helpers.js
@@ -2,4 +2,14 @@ function getRandomNumber(maximum, minimum = 0) {
     return Math.floor(Math.random() * maximum) + minimum;
 }
 
-export { get };
+function debounce(callbackFunction, wait = 100) {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            callbackFunction.apply(this, args);
+        }, wait);
+    };
+}
+
+export { getRandomNumber, debounce };
