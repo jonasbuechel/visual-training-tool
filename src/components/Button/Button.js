c27a77312f55c2272fe0138c414801b418ad1331
jonasbuechel <jonas@frontify.com>
2020-10-08 17:38:57 +0200

    Add simple button component and wire fullscreen toggle

---
+++ src/components/Button/Button.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Button.css';
+
+function Button({text, onClick}) {
+
+    return (
+        <button
+            className="button"
+            onClick={onClick}
+        >{text}</button>
+    );
+}
+
+export default Button;
