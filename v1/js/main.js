ae34ccb027dc84030aecc91344146a25fb74ac03
jonasbuechel <jonas@frontify.com>
2020-10-26 15:52:00 +0100

    Prettify

--- v1/js/main.js
+++ v1/js/main.js
@@ -1,58 +1,68 @@
-import * as SVG from './modules/svg.js';
-import * as Sidebar from './modules/sidebar.js';
-import * as Fullscreen from './modules/fullscreen.js';
-import * as Cursor from './modules/cursor.js';
+import * as SVG from "./modules/svg.js";
+import * as Sidebar from "./modules/sidebar.js";
+import * as Fullscreen from "./modules/fullscreen.js";
+import * as Cursor from "./modules/cursor.js";
 
 /* GET THE SVG */
 window.addEventListener("load", async () => {
   Sidebar.show();
-  await SVG.asyncLoadShape('8-figure');
+  await SVG.asyncLoadShape("8-figure");
   SVG.showCircle();
   SVG.showPaths();
 
   setTimeout(() => {
     Sidebar.hide();
     Cursor.autoHide();
-  }, 2000)
+  }, 2000);
 });
 
-document.addEventListener('keypress', (event) => {
+document.addEventListener("keypress", (event) => {
   SVG.showNextCircle();
 });
 
 /* SETTINGS */
-document.querySelector('#color-intensity-circle').addEventListener('input', (event) => {
-  SVG.updateCircleColor(event.currentTarget.value);
-  SVG.showCircle();
-});
+document
+  .querySelector("#color-intensity-circle")
+  .addEventListener("input", (event) => {
+    SVG.updateCircleColor(event.currentTarget.value);
+    SVG.showCircle();
+  });
 
-document.querySelector('#color-intensity-lines').addEventListener('input', (event) => {
-  SVG.updatePathColor(event.currentTarget.value);
-  SVG.showPaths();
-});
+document
+  .querySelector("#color-intensity-lines")
+  .addEventListener("input", (event) => {
+    SVG.updatePathColor(event.currentTarget.value);
+    SVG.showPaths();
+  });
 
 /* FULLSCREEN */
-document.querySelector('.js-button-toggle-fullscreen').addEventListener('click', (event) => {
-  event.currentTarget.blur();
-  event.currentTarget.classList.toggle('button--state-active');
-  Fullscreen.toggle();
-});
+document
+  .querySelector(".js-button-toggle-fullscreen")
+  .addEventListener("click", (event) => {
+    event.currentTarget.blur();
+    event.currentTarget.classList.toggle("button--state-active");
+    Fullscreen.toggle();
+  });
 
-document.querySelector('.js-button-toggle-intervalmode').addEventListener('click', (event) => {
-  event.currentTarget.blur();
-  event.currentTarget.classList.toggle('button--state-active');
-  SVG.toggleAutoMode(5000);
-});
+document
+  .querySelector(".js-button-toggle-intervalmode")
+  .addEventListener("click", (event) => {
+    event.currentTarget.blur();
+    event.currentTarget.classList.toggle("button--state-active");
+    SVG.toggleAutoMode(5000);
+  });
 
 /* CIRCLE SEQUENCE ORDER */
-document.querySelector('.js-settings__sequence-order').addEventListener('click', (event) => {
-  event.currentTarget.blur();
-  SVG.updateOrder(!event.currentTarget.checked);
-});
+document
+  .querySelector(".js-settings__sequence-order")
+  .addEventListener("click", (event) => {
+    event.currentTarget.blur();
+    SVG.updateOrder(!event.currentTarget.checked);
+  });
 
 /* LOAD SCENE */
-document.querySelectorAll('.js-settings__shape').forEach((input) => {
-  input.addEventListener('click', async (event) => {
+document.querySelectorAll(".js-settings__shape").forEach((input) => {
+  input.addEventListener("click", async (event) => {
     await SVG.asyncLoadShape(event.currentTarget.value);
     SVG.showCircle();
     SVG.showPaths();
@@ -60,22 +70,24 @@ document.querySelectorAll('.js-settings__shape').forEach((input) => {
 });
 
 /* COLOR SCHEME */
-document.querySelectorAll('.js-settings__color-scheme').forEach((input) => {
-  input.addEventListener('click', async (event) => {
-    const bodyNode = document.querySelector('body');
+document.querySelectorAll(".js-settings__color-scheme").forEach((input) => {
+  input.addEventListener("click", async (event) => {
+    const bodyNode = document.querySelector("body");
 
-    if (event.currentTarget.value === 'color-scheme-bright') {
-      bodyNode.classList.add('theme-bright');
-      SVG.setColorScheme('bright');
-    }
-    else {
-      bodyNode.classList.remove('theme-bright');
-      SVG.setColorScheme('dark');
+    if (event.currentTarget.value === "color-scheme-bright") {
+      bodyNode.classList.add("theme-bright");
+      SVG.setColorScheme("bright");
+    } else {
+      bodyNode.classList.remove("theme-bright");
+      SVG.setColorScheme("dark");
     }
 
     /* update settings */
-    document.getElementById('color-intensity-circle').value = SVG.getCircleColorIntensity();
-    document.getElementById('color-intensity-lines').value = SVG.getPathColorIntensity();
+    document.getElementById(
+      "color-intensity-circle"
+    ).value = SVG.getCircleColorIntensity();
+    document.getElementById(
+      "color-intensity-lines"
+    ).value = SVG.getPathColorIntensity();
   });
 });
-
